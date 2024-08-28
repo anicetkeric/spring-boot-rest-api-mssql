941fbf0a1a4e672241cbfcbbb4b7208033487ee6
aek <anicetkeric@outlook.com>
2024-08-28 19:41:30 +0200

    spring boot project initialization

---
+++ src/main/java/com/bootlabs/api/SpringBootRestApiMssqlApplication.java
@@ -0,0 +1,13 @@
+package com.bootlabs.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestApiMssqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestApiMssqlApplication.class, args);
+	}
+
+}
